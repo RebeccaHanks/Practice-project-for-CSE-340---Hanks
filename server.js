4ef9dc4f7809b5919a6a942f3336dbdceaa270cc
RebeccaHanks <han22023@byui.edu>
2026-04-28 21:49:59 -0600

    committing changes

---
+++ server.js
@@ -0,0 +1,24 @@
+// Import express using ESM syntax
+import express from 'express';
+
+// Create an instance of an Express application
+const app = express();
+
+const name = process.env.NAME;
+
+// Define a route handler for the root URL ('/')
+app.get('/', (req, res) => {
+    console.log("Browser requested the home page");
+    res.send(`Welcome, ${name}!`); //name
+});
+
+console.log("NAME is:", process.env.NAME);
+
+// Define the port number the server will listen on
+const PORT = 3000;
+
+// Start the server and listen on the specified port
+app.listen(PORT, () => {
+    console.log(`Server is running on http://127.0.0.1:${PORT}`);
+});
+
